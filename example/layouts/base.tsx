b0b208de8982d206046d7a087466bdc21dc6a6eb
Gabriel Garcia <gabriel@garciat.com>
2026-09-03 22:15:31 +0200

    lowercase

--- example/layouts/base.tsx
+++ example/layouts/base.tsx
@@ -8,7 +8,7 @@ type BaseLayoutProps = {
 export const BaseLayout: React.FC<BaseLayoutProps> = ({ title, children }) => (
   <html>
     <head>
-      <meta charSet="UTF-8" />
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       <title>{title}</title>
